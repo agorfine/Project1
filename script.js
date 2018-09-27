90a229d7fcfde51b1145006a8f1998f6bee427a1
Amanda Gorfine <amandagorfine@ool-ad0203d0.dyn.optonline.net>
2018-09-27 16:02:05 -0400

    completed game- still need to tweak some CSS styling

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
+let intro1 = document.querySelector('.intro1')
+let nextButton = document.querySelector('.next')
+let intro2 = document.querySelector('.intro2')
+let playButton = document.querySelector('.playButton')
+let loadPageContainer = document.querySelector('.loadPageContainer')
 
-// mouse hover function
+
+function next () {
+
+	intro1.classList.add('hidden')
+	intro1.classList.remove('intro1')
+	nextButton.classList.add('hidden')
+	nextButton.classList.remove('next')
+	intro2.classList.add('intro2')
+	intro2.classList.remove('hidden')
+	playButton.classList.add('playButton')
+	playButton.classList.remove('hidden')
+}
+
+nextButton.addEventListener('click', next)
+
+function loadGame() {
+	let board = document.querySelector('.board')
+	let h1 = document.querySelector('h1')
+
+	board.classList.remove('hidden')
+	h1.classList.remove('hidden')
+	loadPageContainer.classList.add('hidden')
+}
+
+playButton.addEventListener('click', loadGame)
+
 function playGame (event) {
 	let square = event.target.id
-
-	// console.log(event.target.id)
-	// console.log(A1)
 	
 	if (document.getElementById(event.target.id) === A1) {
 		document.getElementById(event.target.id).classList.add('boat1Part1')
@@ -45,16 +72,16 @@ let winningSquares = ['A1', 'A2', 'A3', 'B5', 'C5', 'D5', 'D2', 'D3']
 let checkWinningSquares = []
 
 function checkWinner() {
-	let p= document.querySelector('p')
-	let playAgain = document.querySelector('button')
+	let resetText= document.querySelector('#resetText')
+	let resetButton = document.querySelector('#resetButton')
 	let container = document.querySelector('#playAgain')
 
 	if (winningSquares.length === checkWinningSquares.length) {
-		playAgain.classList.add('playAgain')
-		p.textContent = "Congratulations! You helped Queen Elsa defeat the Duke of Weselton!"
-		p.classList.add('winningScreen')
+		resetButton.classList.add('playAgain')
+		resetText.textContent = "Congratulations! You helped Queen Elsa defeat the Duke of Weselton!"
+		resetText.classList.add('winningScreen')
 		container.classList.add('playAgainContainer')
-		playAgain.textContent = "Play Again"
+		resetButton.textContent = "Play Again"
 	}
 
 	let A1 = document.querySelector('#A1')
@@ -77,39 +104,45 @@ function checkWinner() {
 	let D3 = document.querySelector('#D3')
 	let D4 = document.querySelector('#D4')
 	let D5 = document.querySelector('#D5')
-	
+	let E1 = document.querySelector('#E1')
+	let E2 = document.querySelector('#E2')
+	let E3 = document.querySelector('#E3')
+	let E4 = document.querySelector('#E4')
+	let E5 = document.querySelector('#E5')
 
 	function resetGame () {
-		playAgain.classList.remove('playAgain')
-		p.classList.remove('winningScreen')
-		container.classList.remove('playAgainContainer')
-		A1.classList.remove('boat1Part1')
-		A2.classList.remove('boat1Part2')
-		A3.classList.remove('boat1Part3')
-		A4.classList.remove('snowflake')
-		A5.classList.remove('snowflake')
-		B1.classList.remove('snowflake')
-		B2.classList.remove('snowflake')
-		B3.classList.remove('snowflake')
-		B4.classList.remove('snowflake')
-		B5.classList.remove('boat2Part1')
-		C1.classList.remove('snowflake')
-		C2.classList.remove('snowflake')
-		C3.classList.remove('snowflake')
-		C4.classList.remove('snowflake')
-		C5.classList.remove('boat2Part2')
-		D1.classList.remove('snowflake')
-		D5.classList.remove('boat2Part3')
-		D2.classList.remove('boat3Part1')
-		D3.classList.remove('boat3Part2')
-		D4.classList.remove('snowflake')
-		
+		resetButton.classList.remove('playAgain');
+		resetText.classList.remove('winningScreen');
+		container.classList.remove('playAgainContainer');
+		A1.classList.remove('boat1Part1');
+		A2.classList.remove('boat1Part2');
+		A3.classList.remove('boat1Part3');
+		A4.classList.remove('snowflake');
+		A5.classList.remove('snowflake');
+		B1.classList.remove('snowflake');
+		B2.classList.remove('snowflake');
+		B3.classList.remove('snowflake');
+		B4.classList.remove('snowflake');
+		B5.classList.remove('boat2Part1');
+		C1.classList.remove('snowflake');
+		C2.classList.remove('snowflake');
+		C3.classList.remove('snowflake');
+		C4.classList.remove('snowflake');
+		C5.classList.remove('boat2Part2');
+		D1.classList.remove('snowflake');
+		D5.classList.remove('boat2Part3');
+		D2.classList.remove('boat3Part1');
+		D3.classList.remove('boat3Part2');
+		D4.classList.remove('snowflake');
+		E1.classList.remove('snowflake');
+		E2.classList.remove('snowflake');
+		E3.classList.remove('snowflake');
+		E4.classList.remove('snowflake');
+		E5.classList.remove('snowflake');
 		checkWinningSquares = []
-
 	}
-
 	playAgain.addEventListener('click',resetGame)
-}
+};
 
 
 
